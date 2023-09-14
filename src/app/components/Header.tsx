db68979a1f86a28a10d2a52dc75eab477c841645
RKALAIS7 <rkalais7@ford.com>
2023-09-15 01:54:26 +0530

    initial commmit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+const Header: React.FC = () => {
+    console.log('data', Array.from(Array(5)))
+    return (
+        <div className="grid grid-cols-12 gap-4 m-4">
+           <div className="col-span-2">
+                <Image src='./next.svg' alt='' width={100} height={100}/>
+           </div>
+           <div className="col-span-6">
+                <input type="text" placeholder="search" className=" border border-red-300 w-100 h-10 ring-pink-500"/>
+            </div>
+            <div className="col-span-4">
+                <ul className="flex gap-4 flex-row-reverse">
+                    <li>orders</li>
+                    <li>track orders</li>
+                    <li>signin</li>
+                </ul>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+export default function Home() {
+ return (
+	<>
+		<Header />
+		<div className='text-center text-sm hover:text-base'>
+			Hi, I&apos;m under development
+		</div>
+	</>
+ )
+}
